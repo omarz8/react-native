eaed6feac6adb4cdf8ff152891db9e22d92b1252
unknown <8omarzarate@gmail.com>
2021-02-20 09:00:10 -0300

    90% of project done

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, Button, Alert, TouchableOpacity } from 'react-native';
+import crown from './assets/crown3.png';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+
+  const[selectedImage, setSelectedImage]= useState(null)
+
+
+let openImagePickerAsync= async () => {
+ let permissionResult= await ImagePicker.requestMediaLibraryPermissionsAsync()
+ if (permissionResult.granted === false){
+   alert('Permission to camera is required');
+   return;
+ }
+ const pickerResult = await ImagePicker.launchImageLibraryAsync()
+ 
+ if (pickerResult.cancelled === true){
+   return;
+ }
+
+ setSelectedImage({localUri: pickerResult.uri})
+
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+      source={{uri: selectedImage !== null ? selectedImage.localUri : crown}}
+      // source={crown}
+      style={styles.image}
+       >
+      </Image>
+      <Text style={styles.title}> Pick an image!</Text>
+      <TouchableOpacity
+      style={styles.button}
+        onPress= {openImagePickerAsync}
+      >
+        <Text style={styles.buttonText}>
+          Select
+        </Text>
+      </TouchableOpacity>
     </View>
+    
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#292929',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {fontSize: 30, color: '#fff'},
+  image: {height:180, width: 180},
+  button:{
+    backgroundColor:'#590101',
+    marginTop: 10,
+    padding: 7,
+    borderRadius: 50,
+    height:40,
+    width:90,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: '#fff', 
+    fontSize: 20,
+  }
 });
